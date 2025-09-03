f878ba7ae5c38d1eec8f0511fbf8d0b466497683
Bruno Scherer <bruno.raphael96@gmail.com>
2025-09-02 21:01:23 -0300

    fix CORS issue

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,11 +1,29 @@
 import Fastify from 'fastify';
 import helmet from '@fastify/helmet';
+import cors from '@fastify/cors';
 import { questsRoutes } from './features/quests/routes';
 
 export function buildServer() {
   const server = Fastify({ logger: true });
   server.register(helmet);
   server.register(questsRoutes, { prefix: '/api/quests' });
+  server.register(cors, {
+    origin: (origin, cb) => {
+      if (!origin) return cb(null, true);
+
+      const allowedOrigins = [
+        'http://localhost:5173',
+      ];
+
+      if (allowedOrigins.includes(origin)) {
+        cb(null, true);
+      } else {
+        cb(new Error("Not allowed by CORS"), false);
+      }
+    },
+    credentials: true,
+  });
+
   return server;
 }
 
